0e72cbd58e4c8ed22d7b5c066baea1bd11f69749
Lucas Hart <lukke100@gmail.com>
2012-08-11 02:35:11 -0400

    I am so close to completing the tile selection. I need to sleep however. There are notes for future completion in the main.py. Good night.

--- main.py
+++ main.py
@@ -1,46 +1,47 @@
-import pygame, sys, random, time, PIL
+import pygame, sys, random, time
 from pygame.locals import *
-from PIL import Image
 
 pygame.init()
 fpsClock = pygame.time.Clock()
 
-res = 640, 480
+tileSizeX = 32
+tileSizeY = 32
+res = 16, 16
 
 greyColor = pygame.Color(127, 127, 127)
 orangeColor = pygame.Color(255, 127, 0)
 purpleColor = pygame.Color(127, 0, 127)
 whiteColor = pygame.Color(255, 255, 255)
 blackColor = pygame.Color(0, 0, 0)
-windowObj = pygame.display.set_mode(res)
+windowObj = pygame.display.set_mode((res[0] * tileSizeX, res[1] * tileSizeY))
 pygame.display.set_caption("rPepG")
 
 fontObj = pygame.font.Font('freesansbold.ttf', 16)
 
-tileSize = 32
-im = Image.open("tileMap.bmp")
-bitMap = im.load()
+tiles = pygame.image.load("tiles32.png")
+map = pygame.image.load("tileMap.bmp")
+tilesArray = pygame.surfarray.pixels2d(tiles)
+#mapArray = pygame.surfarray.array2d(tileMap) #This is the original version that makes a separate array, independent of the original surface
+mapArray = pygame.surfarray.pixels2d(map) #This method locks the surface and also cannot use 24-bit data
+#However, it is much much faster because the array references to the original Surface. Therefore if you change the array, you change the surface
 
-xTiles = int(res[0]/tileSize)
-yTiles = int(res[1]/tileSize)
+#Future Note: Create two variables that hold the width/tileSize and the length/tileSize so we can accurately sort through what index to choose.
 
-class cameraPosition:
-    def __init__(self, initX, initY):
-        self.xView = initX
-        self.yView = initY
+def getTile(index):
+    #Select the tileSize * tileSize part of the tileArray array that corresponds to the index of the mapArray, this is also dependent of the two variables to sort through
+    #the tiles Array. Also, it may be beneficial to create an array on start up that will map the index value to x and y coordinates on the mapArray. I'm going to sleep
+    surfaceObj = pygame.surfarray.make_surface()
+    return surfaceObj
 
 while True:
+    windowObj.fill(greyColor)
     fpsDisplayObj = fontObj.render("%i" % (fpsClock.get_fps()), False, purpleColor)
     
-    for x in range(0, xTiles):
-        for y in range(0, yTiles):
-            if bitMap[x, y] == 113:
-                pygame.draw.rect(windowObj, whiteColor, (x*tileSize, y*tileSize, tileSize, tileSize), 0)
-            elif bitMap[x, y] == 0:
-                pygame.draw.rect(windowObj, blackColor, (x*tileSize, y*tileSize, tileSize, tileSize), 0)
-            #Use a indexing to assign bitMap values to display certain colors/tiles rather than a shit tonne
-            #of if statements.
-    windowObj.blit(fpsDisplayObj,(0, 0))
+    for x in range(0, res[0]):
+        for y in range(0, res[1]):
+            windowObj.blit(getTile(mapArray[x][y]), (x * tileSizeX, y * tileSizeY))
+
+    windowObj.blit(fpsDisplayObj, (0, 0))
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
