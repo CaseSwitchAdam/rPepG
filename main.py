0432dd272ab850f6a39b7a371b89ef7c5ecee8ff
Lucase Hart <lukke100@gmail.com>
2012-08-26 14:39:31 -0400

    Finised per-tile movement. Holy hell.

--- main.py
+++ main.py
@@ -54,13 +54,21 @@ class Entity:
         self.ySize = ySize
         self.xTile = xPos / tileSize
         self.yTile = yPos / tileSize
+        self.offsetX = self.xCoord % tileSize
+        self.offsetY = self.yCoord % tileSize
         self.tangible = tangible
         
-    def updatePos(self, xPos, yPos):
+    def updateXPos(self, xPos):
         self.xCoord = xPos
-        self.yCoord = yPos
         self.xTile = xPos / tileSize
+        self.offsetX = self.xCoord % tileSize
+        self.offsetY = self.yCoord % tileSize
+        
+    def updateYPos(self, yPos):
+        self.yCoord = yPos
         self.yTile = yPos / tileSize
+        self.offsetX = self.xCoord % tileSize
+        self.offsetY = self.yCoord % tileSize        
         
     def checkXAlign(self):
         if self.xCoord % tileSize:
@@ -73,6 +81,9 @@ class Entity:
             return True
         else:
             return False
+            
+    xVelocity = 0
+    yVelocity = 0
         
         
 class Character(Entity):
@@ -88,9 +99,66 @@ class Character(Entity):
     yShift = 0
     
     def shift(self):
-        #Alright, so you must make this shift by the speed per frame and also restrict to orthagonal movement.
-        #If 0 < tileSize - Coordinate % tileSize <= speed: then snap to next grid.
-        #YOU CAN DO THIS!
+        if self.xVelocity == self.yVelocity == 0:
+            if self.xShift == -1:
+                if 0 < self.offsetX <= self.speed:
+                    self.updateXPos(self.xCoord - self.offsetX)
+                    self.xShift = 0
+                else:
+                    self.updateXPos(self.xCoord - self.speed)
+            elif self.xShift == 1:
+                if 0 < (tileSize - self.offsetX) <= self.speed:
+                    self.updateXPos(self.xCoord + (tileSize - self.offsetX))
+                    self.xShift = 0
+                else:
+                    self.updateXPos(self.xCoord + self.speed)
+            elif self.yShift == -1:
+                if 0 < self.offsetY <= self.speed:
+                    self.updateYPos(self.yCoord - self.offsetY)
+                    self.yShift = 0
+                else:
+                    self.updateYPos(self.yCoord - self.speed)
+            elif self.yShift == 1:
+                if 0 < (tileSize - self.offsetY) <= self.speed:
+                    self.updateYPos(self.yCoord + (tileSize - self.offsetY))
+                    self.yShift = 0
+                else:
+                    self.updateYPos(self.yCoord + self.speed)
+        elif self.xVelocity != 0:
+            if self.xShift == 0:
+                self.xShift = math.copysign(1, self.xVelocity)
+                self.updateXPos(self.xVelocity + self.xCoord)
+            else:
+                if self.xShift == -1:
+                    if 0 < self.offsetX <= self.speed:
+                        self.updateXPos(self.xCoord - self.offsetX)
+                        self.xShift = 0
+                    else:
+                        self.updateXPos(self.xCoord - self.speed)
+                elif self.xShift == 1:
+                    if 0 < (tileSize - self.offsetX) <= self.speed:
+                        self.updateXPos(self.xCoord + (tileSize - self.offsetX))
+                        self.xShift = 0
+                    else:
+                        self.updateXPos(self.xCoord + self.speed)
+                        
+        elif self.yVelocity != 0:
+            if self.yShift == 0:
+                self.yShift = math.copysign(1, self.yVelocity)
+                self.updateYPos(self.yVelocity + self.yCoord)
+            else:
+                if self.yShift == -1:
+                    if 0 < self.offsetY <= self.speed:
+                        self.updateYPos(self.yCoord - self.offsetY)
+                        self.yShift = 0
+                    else:
+                        self.updateYPos(self.yCoord - self.speed)
+                elif self.yShift == 1:
+                    if 0 < (tileSize - self.offsetY) <= self.speed:
+                        self.updateYPos(self.yCoord + (tileSize - self.offsetY))
+                        self.yShift = 0
+                    else:
+                        self.updateYPos(self.yCoord + self.speed)
         
 class Player(Character):
     def __init__(self, health, mana, level, speed, direction, xPos, yPos, xSize, ySize, tangible):
@@ -112,13 +180,12 @@ print "tileSurfaceArray length:", len(tileSurfaceArray)
 camera = Camera(0, 0)
 
 monster = Monster(10, 10, 5, 4, 0, 128, 128, 32, 32, 1)
-player = Player(20, 20, 10, 4, 0, 256, 256, 32, 32, 1)
+player = Player(20, 20, 10, 1, 0, 256, 256, 32, 32, 1)
 
 while True:
     fpsDisplayObj = fontObj.render("%i" % (fpsClock.get_fps()), False, purpleColor)
-    camDisplayObj = fontObj.render("%r, %r" % ((player.xCoord), (player.yCoord)), False, whiteColor)
-    camTileXDisplayObj = fontObj.render("%r, %r" % ((player.xTile), (player.yTile)), False, whiteColor)
-    camTileXDisplayObj = fontObj.render("%r, %r" % ((player.xTile), (player.yTile)), False, whiteColor)
+    posDisplayObj = fontObj.render("%r, %r" % ((player.xCoord), (player.yCoord)), False, whiteColor)
+    posTileXDisplayObj = fontObj.render("%r, %r" % ((player.xTile), (player.yTile)), False, whiteColor)
 
     player.shift()
     camera.updatePos(player.xCoord - res[0] / 2 + player.xSize / 2, player.yCoord - res[1] / 2 + player.ySize / 2)
@@ -129,12 +196,12 @@ while True:
             
     pygame.draw.rect(windowObj, whiteColor, (player.xCoord - camera.xCoord, player.yCoord - camera.yCoord, player.xSize, player.ySize))
     
-    if monster.xCoord in range(camera.xCoord, camera.xCoord + res[0]) and monster.yCoord in range(camera.yCoord, camera.yCoord + res[1]):
+    if monster.xCoord in range(camera.xCoord - monster.xSize, camera.xCoord + res[0]) and monster.yCoord in range(camera.yCoord - monster.ySize, camera.yCoord + res[1]):
         pygame.draw.rect(windowObj, blackColor, (monster.xCoord - camera.xCoord, monster.yCoord - camera.yCoord, monster.xSize, monster.ySize))
     
     windowObj.blit(fpsDisplayObj, (0, 0))
-    windowObj.blit(camDisplayObj, (0, 16))
-    windowObj.blit(camTileXDisplayObj, (0, 32))
+    windowObj.blit(posDisplayObj, (0, 16))
+    windowObj.blit(posTileXDisplayObj, (0, 32))
     
     for event in pygame.event.get():
         if event.type == QUIT:
@@ -143,23 +210,23 @@ while True:
             
         elif event.type == KEYUP:
             if event.key == K_DOWN:
-                player.yShift -= player.speed
-            if event.key == K_UP:
-                player.yShift += player.speed
-            if event.key == K_LEFT:
-                player.xShift -= player.speed
-            if event.key == K_RIGHT:
-                player.xShift += player.speed
+                player.yVelocity -= player.speed
+            elif event.key == K_UP:
+                player.yVelocity += player.speed
+            elif event.key == K_LEFT:
+                player.xVelocity += player.speed
+            elif event.key == K_RIGHT:
+                player.xVelocity -= player.speed
         
         elif event.type == KEYDOWN:
             if event.key == K_DOWN:
-                player.yShift += player.speed
-            if event.key == K_UP:
-                player.yShift -= player.speed
-            if event.key == K_LEFT:
-                player.xShift += player.speed
-            if event.key == K_RIGHT:
-                player.xShift -= player.speed
+                player.yVelocity += player.speed
+            elif event.key == K_UP:
+                player.yVelocity -= player.speed
+            elif event.key == K_LEFT:
+                player.xVelocity -= player.speed
+            elif event.key == K_RIGHT:
+                player.xVelocity += player.speed
             
     pygame.display.update()
     fpsClock.tick(60)
